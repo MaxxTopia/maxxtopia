fe7af6060555f5d608d113b094ecbd53431cc0e1
Diggy <276703970+dtman-gif@users.noreply.github.com>
2026-06-28 18:51:38 -0700

    Sprite Cannon mobile fixes: name-input restart guard, button visibility, iOS centering, audio unlock, panels fit; SW v3

--- public/play/spritecannon/sw.js
+++ public/play/spritecannon/sw.js
@@ -2,7 +2,7 @@
    Scope = the directory this file is served from (e.g. /play/spritecannon/).
    Bump CACHE when shipping a new build so clients pull the fresh shell. */
 "use strict";
-var CACHE = "sprite-cannon-v2";
+var CACHE = "sprite-cannon-v3";
 var SHELL = [
   "./",
   "index.html",
